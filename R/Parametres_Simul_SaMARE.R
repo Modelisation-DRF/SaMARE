bfa2d3c6299bff88752ff359199d9d7b169c26ba
Peumi <Junior.Peumi@mrnf.gouv.qc.ca>
2024-01-25 15:14:50 -0500

    mise a jour des Data

--- R/Parametres_Simul_SaMARE.R
+++ R/Parametres_Simul_SaMARE.R
@@ -8,27 +8,27 @@
 
 ParametresSimulSaMARE <- function() {
 
-  CovParms<-read_delim("Parametres/0_MatchModuleCovparms.csv", delim=";")
-  EfCovParms<-read_delim("Parametres/EffetCovparms.csv", delim=";")
+  CovParms<-MatchModuleCovparms
+  EfCovParms<-EffetCovparms
 
-  CovParmsGaules<-read_delim("Parametres/CovparmGaules.csv", delim=";")
+  CovParmsGaules<-CovparmGaules
 
   ####### Fichier des parametres
-  Para<-read_delim("Parametres/0_MatchModuleParameters.csv", delim=";")
+  Para<-MatchModuleParameters
 
-  ParaGaules<-read_delim("Parametres/ParametresGaules.csv", delim=";")
+  ParaGaules<-ParametresGaules
 
 
   # Fichier des especes
-  Sp<-read_delim("Parametres/0_Species.csv", delim=";")
-  SpGroups<-read_delim("Parametres/0_SpeciesGroups.csv", delim=";")
+  Sp<-Species.csv
+  SpGroups<-SpeciesGroups
   # Fichier des especes dans chacun des groupes d'especes
-  MatchSpGroups<-read_delim("Parametres/0_MatchSpeciesGroups.csv", delim=";")
+  MatchSpGroups<-MatchSpeciesGroups
 
   #Omega
-  Omega<-read_delim("Parametres/0_MatchModuleOmega.csv", delim=";")
+  Omega<-MatchModuleOmega
 
-  OmegaGaules<-read_delim("Parametres/OmegaGaulesFormat.csv", delim=",")
+  OmegaGaules<-OmegaGaulesFormat
 
   List_Para <- list(CovParms,EfCovParms, CovParmsGaules, Para, ParaGaules, Sp, SpGroups, MatchSpGroups, Omega, OmegaGaules )
   return(List_Para)
