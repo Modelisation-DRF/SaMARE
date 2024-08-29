8625121fcdd33c2fcc593dbcf0b9b305214cb6ef
Peumi <Junior.Peumi@mrnf.gouv.qc.ca>
2024-08-29 15:37:37 -0400

    mise à jour

--- R/Sortie_Billonage.R
+++ R/Sortie_Billonage.R
@@ -21,8 +21,20 @@ SortieBillonage <- function(Data, Type ){
 
   billo <- SIMBillonnageABCD_DHP(data1, Type)
 
-  final <- left_join(data1, billo, by = "bilonID")
-  final <- final %>% select(-bilonID)
+  final <- left_join(data1, billo, by = "bilonID") %>%  mutate (Stm2ha=pi*(DHPcm/200)^2,
+                                                                Vigueur = case_when(
+                                                                  vigu0 == "ViG" & prod0 == "sciage" ~ 1,
+                                                                  vigu0 == "ViG" & prod0 == "pate" ~ 2,
+                                                                  vigu0 == "NONVIG" & prod0 == "sciage" & DHPcm>=23.1 ~ 3,
+                                                                  vigu0 == "NONVIG" & prod0 == "sciage" & DHPcm<23.1 ~ 4,
+                                                                  vigu0 == "NONVIG" & prod0 == "pate" ~ 4,
+                                                                  vigu0 == "ViG" & prod0 == "resineux" ~ 5,
+                                                                  vigu0 == "NONVIG" & prod0 == "resineux" ~ 6,
+                                                                  TRUE ~ NA_integer_
+                                                                ))
+
+  final <- final %>% select(Annee,Residuel,ArbreID,Iter,NoArbre,Placette,Nombre,GrEspece,Espece,
+                            Etat,DHPcm,MSCR,hauteur_pred,vol_dm3,Stm2ha,Sup_PE,ABCD,Vigueur,DER,F1,F2,F3,F4,P,type)
 
 
   return(final)
