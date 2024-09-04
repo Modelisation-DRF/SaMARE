e3f4217b27515434664cd23dd0fd69c793aaa61d
Peumi <Junior.Peumi@mrnf.gouv.qc.ca>
2024-09-04 10:35:43 -0400

    test de pour samare

--- tests/testthat/test-Sortie_Dendro_Samare.R
+++ tests/testthat/test-Sortie_Dendro_Samare.R
@@ -1,3 +1,5 @@
 test_that("SortieDendroSamare return the expected data frame ", {
   expect_equal(SortieDendroSamare(SimulHtVol = expect_for_Simulateur_Samare_sans_gaules_et_coupe_test), expect_for_sortie_dendro_samare_test )
 })
+
+
